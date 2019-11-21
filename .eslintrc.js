861da2ace26c831962a3a15912b6461ba2b3fe84
Erwan Mahe <er1mahe@gmail.com>
2019-11-21 22:30:39 +0100

    chore: setup eslint, prettier config
    Signed-off-by: Erwan Mahe <er1mahe@gmail.com>

--- .eslintrc.js
+++ .eslintrc.js
@@ -3,12 +3,19 @@ module.exports = {
   env: {
     node: true
   },
-  extends: ["plugin:vue/essential", "@vue/prettier"],
+  extends: [
+    'plugin:vue/essential',
+    '@vue/prettier',
+    'plugin:prettier/recommended'
+  ],
   rules: {
-    "no-console": process.env.NODE_ENV === "production" ? "error" : "off",
-    "no-debugger": process.env.NODE_ENV === "production" ? "error" : "off"
+    'no-console': process.env.NODE_ENV === 'production' ? 'error' : 'off',
+    'no-debugger': process.env.NODE_ENV === 'production' ? 'error' : 'off',
+    'prettier/prettier': 'error'
   },
+  parser: 'vue-eslint-parser',
   parserOptions: {
-    parser: "babel-eslint"
+    parser: 'babel-eslint',
+    sourceType: 'module'
   }
-};
+}
